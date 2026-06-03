2104849467594cb16e001921239150694523b6db
BandojuSwathiLaxmi <swathilakshmi508@gmail.com>
2026-06-03 13:09:24 +0530

    Cognizant Upskilling Tasks

--- UPSKILL/Module_3_CoreJava/ThreadCreation.java
+++ UPSKILL/Module_3_CoreJava/ThreadCreation.java
@@ -5,7 +5,7 @@ class MyThread extends Thread{
         }
     }
 }
-public class ThreadExample{
+public class ThreadCreation{
     public static void main(String[] args) {
         MyThread t1=new MyThread();
         MyThread t2=new MyThread();
